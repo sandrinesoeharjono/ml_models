a8e6bc46e0e4bae3fde9c78ea9eb1ac8d52c5b7c
Sandrine Soeharjono <sandrinesoeharjono@hotmail.com>
2023-02-23 16:23:50 -0500

    Add script to process data, create/train model.

---
+++ image_classification/data.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+working_dir = Path.cwd()
+filter_corrupted_data = True
+
+if filter_corrupted_data:
+    print("Removing corrupted images...")
+    num_skipped = 0
+    for folder_name in ["cat", "dog"]:
+        folder_path = working_dir / "data" / "pet_images" / folder_name
+        for fname in folder_path.glob("*.jpg"):
+            fpath = folder_path / fname
+            try:
+                fobj = open(fpath, "rb")
+                is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+            finally:
+                fobj.close()
+
+            if not is_jfif:
+                num_skipped += 1
+                os.remove(fpath)
+    print(f"Deleted {num_skipped} corrupted images.")
+
+# Generate a dataset
+print("Generating train & test sets from repository...")
+train_data, test_data = tf.keras.utils.image_dataset_from_directory(
+    "data/pet_images",
+    validation_split=0.2,
+    subset="both",
+    seed=1337,
+    image_size=(180, 180),
+    batch_size=150,
+)
+
+# Apply data augmentation to the training dataset
+print("Augment training set by adding random granularity (flipping & rotating images)...")
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+)
+train_data = train_data.map(
+    lambda img, label: (data_augmentation(img), label),
+    num_parallel_calls=tf.data.AUTOTUNE,
+)
+
+# Prefetch samples in GPU memory helps maximize GPU utilization
+train_data = train_data.prefetch(tf.data.AUTOTUNE)
+val_ds = test_data.prefetch(tf.data.AUTOTUNE)
---
+++ image_classification/model.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.utils import plot_model
+
+from data import image_size, train_data, test_data
+
+print("Defining the model...")
+def make_model(input_shape, num_classes):
+    inputs = keras.Input(shape=input_shape)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(inputs)
+    x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+model = make_model(input_shape=image_size + (3,), num_classes=2)
+plot_model(model, show_shapes=True)
+
+# Train the model
+print(f"Beginning model training...")
+epochs = 25
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("save_at_{epoch}.keras"),
+]
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-3),
+    loss="binary_crossentropy",
+    metrics=["accuracy"],
+)
+model.fit(
+    train_data,
+    epochs=epochs,
+    callbacks=callbacks,
+    validation_data=test_data,
+)
+
+# Run classification model onto new image
+print("Using model to make prediction onto new image...")
+img = keras.preprocessing.image.load_img(
+    "data/pet_images/cat/6779.jpg", target_size=image_size
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(img_array)
+score = float(predictions[0])
+print(f"This image is {100 * (1 - score):.2f}% cat and {100 * score:.2f}% dog.")
